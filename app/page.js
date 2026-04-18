bbcb3f6cb2950121df4132f7cec8172c94e688a9
YashChandan21 <yashparaschandan4@gmail.com>
2026-04-18 15:56:04 +0530

    change: bg color

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home(){
   const [result, setResult] = useState("");
 
   return (
-    <div className="min-h-screen bg-black text-white p-6">
+    <div className="min-h-screen bg-linear-to-r from-black via-gray-900 to-black  text-white p-6">
       <h1 className="text-3xl font-bold text-center mb-6">
         Prompt Generator
       </h1>
