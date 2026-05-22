1833adfdee9799cf6ad80c80c9cecf228b86ed7b
YashChandan21 <yashparaschandan4@gmail.com>
2026-05-23 01:10:48 +0530

    fix: bg with font

--- app/page.js
+++ app/page.js
@@ -1,20 +1,33 @@
 'use client'
+
 import { useState } from "react"
 import PromptForm from "@/components/PromptForm";
 import OutputBox from "@/components/OutputBox";
+import Image from "next/image";
 
-
-export default function Home(){
+export default function Home() {
   const [result, setResult] = useState("");
 
   return (
-    <div className="min-h-screen bg-linear-to-r from-black via-gray-900 to-black   text-white p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">
-        Prompt Generator
-      </h1>
+    <div className="relative min-h-screen bg-gradient-to-r from-black via-gray-900 to-black text-white p-6">
 
-      <PromptForm setResult={setResult} />
-      <OutputBox result={result} /> 
-    </div>  
-  )
+      {/* Centered Image */}
+      <div className="flex justify-center mt-10">
+        <Image
+          src="/images/PromptImg.png"
+          alt="Prompt Generator"
+          width={500}
+          height={120}
+          className="rounded-xl"
+        />
+      </div>
+
+      {/* Main Content */}
+      <div className="-mt-30">
+        <PromptForm setResult={setResult} />
+        <OutputBox result={result} />
+      </div>
+
+    </div>
+  );
 }
